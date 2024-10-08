72772d6582c374d84b80f849bc2ea1c121279068
st3v39 <152023701+st3v39@users.noreply.github.com>
2024-10-08 07:47:18 -0700

    Add files via upload

---
+++ form.js
@@ -0,0 +1,6 @@
+const mysubmit= document.getElementById("mysubmit").onclick=function(){
+    myp.textContent='Account Created!!'
+}
+var text=document.getElementById('mysubmit').onclick=function(){
+    document.getElementById('label').textContent=label;
+}
